c1a51d78f37b703e9815b3deaa9b0b14a378fd1a
Cipher-20 <kan20_2003@abv.bg>
2022-12-07 00:16:06 +0200

    simple css to body calculator

--- src/components/CalcBody/CalcBody.js
+++ src/components/CalcBody/CalcBody.js
@@ -2,16 +2,18 @@ function CalcBody() {
     return(
         <>
             <div>
-
-            <label >Enter your expression:</label>
-            <input type="text" id="calculator-body" name="calculator-body" />
+            
+            <div className="label-input-number">
+            <label  className="label-expression">Enter your expression:</label>
+            <input  type="text" id="calculator-body" name="calculator-body" />
+            </div>
 
             
 
-            <button>Add</button>
-            <button>Subtraction</button>
-            <button>Multiply</button>
-            <button>Division</button>
+            <button className="btn" >Add</button>
+            <button className="btn" >Subtraction</button>
+            <button className="btn" >Multiply</button>
+            <button className="btn" >Division</button>
 
 
 
