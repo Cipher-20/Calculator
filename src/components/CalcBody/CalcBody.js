bd2687cbfd1bb412376e352fd4c0f6e80904fe87
Cipher-20 <kan20_2003@abv.bg>
2022-12-06 22:29:45 +0200

    simple html skeleton

---
+++ src/components/CalcBody/CalcBody.js
@@ -0,0 +1,26 @@
+function CalcBody() {
+    return(
+        <>
+            <div>
+
+            <label >Enter your expression:</label>
+            <input type="text" id="calculator-body" name="calculator-body" />
+
+            
+
+            <button>Add</button>
+            <button>Subtraction</button>
+            <button>Multiply</button>
+            <button>Division</button>
+
+
+
+
+
+            </div>
+        </>
+    );
+}
+
+
+export default CalcBody;
